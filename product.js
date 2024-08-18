27dc3a6f49538067377c86ec54979e6a6411fc09
Waleedkhttab95 <wa.ez1995@gmail.com>
2024-08-18 14:37:23 +0300

    Refactoring code

--- product.js
+++ product.js
@@ -1,7 +1,17 @@
-app.get('/product/:productId', (req, res) => { 
-    db.query(`SELECT * FROM products WHERE id=${req.params.productId}`, (err, result) => { 
-    if (err) throw err; 
-    res.send(result); 
-    }); 
-    }); 
-    
+app.get("/product/:productId", (req, res) => {
+  const productId = req.body.params;
+  if (!productId) return res.status(400).send("ProductId requeierd !");
+
+  db.query(`SELECT * FROM products WHERE id=?`, [productId], (err, result) => {
+    if (err) {
+      console.log(`DB query for product error : ${err}`);
+      return res.status(500).send("Ops something wrong , Try again"); // we don't need to return any details about the error to client side
+    }
+
+    if (!result) {
+      return res.status(404).send("Product Not Found !");
+    }
+
+    return res.status(200).send(result);
+  });
+});
